009cccd205e5ca02aa92f22aa6320e37e8fea329
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-28 16:25:07 +0900

    イベントの利用

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var app = new Vue({
   el: '#app',
-  data: {
-    // message: 'Hello Vue.js!'
-    list: ['りんご', 'バナナ', 'いちご']
+  // data: {
+  //   message: 'Hello Vue.js!'
+  //   list: ['りんご', 'バナナ', 'いちご']
+  // }
+  methods: {
+    handleClick: function(event) {
+      alert(event.target) 
+    }
   }
 })
