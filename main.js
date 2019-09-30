f4fb12c3184abca6adeed70179c1b8353264fc77
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-30 10:23:19 +0900

    data binding3

--- main.js
+++ main.js
@@ -24,13 +24,29 @@
 
 // dataオプションの直下のプロパティは後から追加できないため、
 // 内容が決まってない場合でも初期値や空データとして定義しておく必要があります。
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     newTodoText: '',
+//     visitCount: 0,
+//     hideCompletedTodos: false,
+//     todos: [],
+//     error: null
+//   }
+// })
+
+// データバインディングでは、ルートに定義したプロパティだけでなく、
+// ネストされているオブジェクトのプロパティや、配列の要素を使用することもできます。
 var app = new Vue({
   el: '#app',
   data: {
-    newTodoText: '',
-    visitCount: 0,
-    hideCompletedTodos: false,
-    todos: [],
-    error: null
+    // オブジェクトデータ
+    message: {
+      value: 'Hello Vue.js!'
+    },
+    // 配列データ
+    list: ['りんご', 'バナナ', 'いちご'],
+    // 別のデータを使用してlistから取り出す要素を動的に
+    num: 1
   }
 })
