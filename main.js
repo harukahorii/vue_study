f41895cb85588413180d9757702500cf2f67d582
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-30 11:47:27 +0900

    object data class and style

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-// クラスとスタイル属性へのデータバインディングは、オブジェクトや配列を
-// バインドするとクラス名やCSSプロパティとして展開されます。
+// 2.p63
+
 
 var app = new Vue({
   el: '#app',
   data: {
-    isChild: true,
-    isActive: true,
-    textColor: 'red',
-    bgColor: 'lightgray'
+    classObject: {
+      child: true,
+      'is-active': false
+    },
+    styleObject: {
+      color: 'red',
+      backgroundColor: 'lightgray'
+    }
   }
   
 })
