7aada398523d1085c011cd1dc3e3171040f52b8f
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-30 12:49:38 +0900

    conditional branch by template

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-// SVGのデータバインディング
+// テンプレートベースで条件分岐
+// v-ifとv-showディレクティブは、付与した要素の描画または表示に条件を適用する
+//  okプロパティがtrueの時のみ<div>要素を表示
 var app = new Vue({
   el: '#app',
   data: {
-    radius: 50
+    ok: false
   }
 })
