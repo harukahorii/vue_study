3c1c5e0294f592d03742738a22a410d812c4b70e
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-30 12:38:15 +0900

    SVG data binding

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-
-// 複数の属性のデータバインディング
+// SVGのデータバインディング
 var app = new Vue({
   el: '#app',
   data: {
-    item: {
-      id: 1,
-      src: 'item1.jpg',
-      alt: '商品サムネイル',
-      width: 200,
-      height: 200
-    }
+    radius: 50
   }
-  
 })
