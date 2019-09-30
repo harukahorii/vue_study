302ca419f9bd309a63124f871941112232b72800
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-30 10:03:11 +0900

    data binding1

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Vue.js!'
+//     list: ['りんご', 'バナナ', 'いちご']
+//     show: true
+//   }
+//   methods: {
+//     handleClick: function(event) {
+//       alert(event.target) 
+//     }
+//   }
+// })
+
+var state = { count: 0 }
 var app = new Vue({
   el: '#app',
   data: {
-    // message: '初期メッセージ'
-  //   list: ['りんご', 'バナナ', 'いちご']
-    show: true
+    state: state
   }
-  // methods: {
-  //   handleClick: function(event) {
-  //     alert(event.target) 
-  //   }
-  // }
 })
+
+state.count++
