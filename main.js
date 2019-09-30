43151a8d1103edf9f163c2d0ceca104cdafa4a72
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-30 10:10:47 +0900

    data binding2

--- main.js
+++ main.js
@@ -12,12 +12,25 @@
 //   }
 // })
 
-var state = { count: 0 }
+// var state = { count: 0 }
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     state: state
+//   }
+// })
+
+// state.count++
+
+// dataオプションの直下のプロパティは後から追加できないため、
+// 内容が決まってない場合でも初期値や空データとして定義しておく必要があります。
 var app = new Vue({
   el: '#app',
   data: {
-    state: state
+    newTodoText: '',
+    visitCount: 0,
+    hideCompletedTodos: false,
+    todos: [],
+    error: null
   }
 })
-
-state.count++
