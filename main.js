4be455e8660c6ad7bf1fd0547d64c93206345053
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-10-01 19:28:13 +0900

    how to

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 var app = new Vue({
   el: '#app',
-  data: {
   
-    list: []
-  },
-  created: function() {
-    axios.get('https://api.myjson.com/bins/1fwqkt').then(function(response){
-      this.list = response.data
-    }.bind(this)).catch(function(e) {
-      console.error(e)
-    })
+  mounted: function() {
+    console.log(this.$refs.hello)
   }
 })
