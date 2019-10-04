f52ffcdd235fe74333cf77705da2a54b25292d85
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-10-04 16:34:24 +0900

    event prevent

--- main5.js
+++ main5.js
@@ -4,8 +4,14 @@ new Vue({
     message: 'Hello Vue.js!'
   },
   methods: {
-    handleInput: function(event) {
-      this.message = event.target.value
+    handler: function(comment) {
+      console.log(comment)
     }
   }
 })
+
+// 通常のHTMLでは、同じイベントをハンドルしたDOMがネストされている場合
+// event.target要素から順に外側に（親要素方向）に向かってハブリング
+// (ターゲットを包含している要素のイベントが連鎖して行われる)します。
+// イベント修飾子を付与することでハブリングを制御できます。
+
