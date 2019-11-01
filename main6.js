5866be79c73829bff1a0fbb284dfa4ede960adfb
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-11-01 13:28:12 +0900

    cmputed propety

--- main6.js
+++ main6.js
@@ -1,11 +1,22 @@
 var app = new Vue({
   el: '#app',
   data: {
-    width: 800
+    width: 800,
+    height: 600
   },
   computed: {
     halfWidth: function() {
       return this.width / 2
+    },
+    halfHeight: function() {
+      return this.height / 2
+    },
+    // 「width * height」の中心座標をオブジェクトで返す
+    halfPoint: function() {
+      return {
+        x: this.halfWidth,
+        y: this.halfHeight
+      }
     }
   }
 })
