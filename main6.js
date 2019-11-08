653af9c3c822a19b58787cf84a8b14b1077196aa
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-11-08 11:39:43 +0900

    getter and setter

--- main6.js
+++ main6.js
@@ -1,22 +1,13 @@
 var app = new Vue({
   el: '#app',
   data: {
-    width: 800,
-    height: 600
+    width: 800
   },
   computed: {
-    halfWidth: function() {
-      return this.width / 2
-    },
-    halfHeight: function() {
-      return this.height / 2
-    },
-    // 「width * height」の中心座標をオブジェクトで返す
-    halfPoint: function() {
-      return {
-        x: this.halfWidth,
-        y: this.halfHeight
-      }
+    halfWidth: {
+      get: function() { return this.width / 2 },
+      // halfWidthの2倍の数値をwidthに入れる
+      set: function(val) { this.width = val * 2 }, 
     }
   }
 })
