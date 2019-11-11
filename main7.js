ee82ecd59f826440736a293d63b2c14378a6b918
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-11-11 11:30:47 +0900

    computed property cache

--- main7.js
+++ main7.js
@@ -1,13 +1,9 @@
 var app = new Vue({
   el: '#app',
-  data: {
-    width: 800
-  },
   computed: {
-    halfWidth: {
-      get: function() { return this.width / 2 },
-      // halfWidthの2倍の数値をwidthに入れる
-      set: function(val) { this.width = val * 2 }, 
-    }
+    computedData: function() { return Math.random() }
+  },
+  methods: {
+    methodsData: function() { return Math.random() }
   }
 })
