02294f2dd59504fd26c3825f4eff905406d8a30f
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-11-13 11:15:58 +0900

    narrow cache

--- main7.js
+++ main7.js
@@ -1,9 +1,29 @@
 var app = new Vue({
   el: '#app',
-  computed: {
-    computedData: function() { return Math.random() }
+  data: {
+  // フォーム入力と紐付けるデータ
+    budget: 300,
+    // 表示数
+    limit: 2,
+    // 元になるリスト
+    list: [
+      { id:1, name:'りんご', price: 100 },
+      { id:2, name:'ばなな', price: 200},
+      { id:3, name:'いちご', price: 400},
+      { id:4, name:'オレンジ', price: 300},
+      { id:5, name:'メロン', price: 500}
+    ]
   },
-  methods: {
-    methodsData: function() { return Math.random() }
+  computed: {
+    // budget以下のリストを返す算出プロパティ
+    matched: function() {
+      return this.list.filter(function(el) {
+        return el.price <= this.budget
+      }, this)
+    },
+    // matchedで返ったデータをlimit件返すプロパティ
+    limited: function() {
+      return this.matched.slice(0, this.limit)
+    }
   }
 })
