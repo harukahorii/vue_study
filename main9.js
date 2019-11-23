b762b16dedcb5b1713a622714c6b1b5e4f05524b
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-11-23 20:36:58 +0900

    Update main9.js

--- main9.js
+++ main9.js
@@ -1,14 +1,11 @@
 var app = new Vue({
   el: '#app',
   data: {
-    value: '編集してみてね'
+    price: 19800
   },
-watch: {
-  value: _.debounce (function (newVal) {
-    // ここへコストの高い処理を書く
-    console.log(newVal)
-  },
-  // valueの変化が終わるのを待つ時間をミリ秒で指定
-  500) 
-}
+  filters: {
+    localeNum: function(val) {
+      return val.toLocalString()
+    }
+  }
 })
