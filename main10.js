e9761fcf1b2530446cc60cf097f8fe41c146eb03
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-11-27 06:17:04 +0900

    update index10.html

--- main10.js
+++ main10.js
@@ -1,13 +1,12 @@
 new Vue({
   el: '#app',
-  filters: {
-    // 小数点以下第二位に丸めるフィルタ
-    round: function(val) {
-      return Math.round(val * 100)/100
-    },
-    // 度からラジアンに変換するフィルタ
-    radian: function(val) {
-      return val * Math.PI / 180
+  directives: {
+    focus: {
+      // 紐づいている要素がDOMに挿入される時
+      inserted: function(el) {
+        // 要素にフォーカスをあてる
+        el.focus()
+      }
     }
   }
 })
