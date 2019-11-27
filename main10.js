997e8e50830ca5e0feeeeb6ad39150f79665353d
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-11-28 06:16:43 +0900

    update index10.html

--- main10.js
+++ main10.js
@@ -2,18 +2,31 @@
 //   el: '#app',
 //   directives: {
 //     focus: {
-//       紐づいている要素がDOMに挿入される時
+//       // 紐づいている要素がDOMに挿入される時
 //       inserted: function(el) {
-//         要素にフォーカスをあてる
+//         // 要素にフォーカスをあてる
 //         el.focus()
+//         console.log(v-focus)
 //       }
 //     }
 //   }
 // })
 
 
-Vue.directive('focus', {
-  inserted: function(el) {
-    el.focus
-  }
+Vue.directive('example', {
+  bind: function(el, binding) {
+    console.log('v-example bind')
+  },
+  inserted: function(el, binding) {
+    console.log('v-example inserted')
+  },
+  update: function(el, binding) {
+    console.log('v-example update')
+  },
+  componentUpdated: function(el, binding) {
+    console.log('v-example componentUpdated')
+  },
+  unbind: function(el, binding) {
+    console.log('v-example unbind')
+  },
 })
