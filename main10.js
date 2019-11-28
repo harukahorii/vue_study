56e916adf0f80d60abde4faaf2563cdbba3e4eb4
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-11-29 06:21:44 +0900

    video playing

--- main10.js
+++ main10.js
@@ -1,32 +1,14 @@
-// new Vue({
-//   el: '#app',
-//   directives: {
-//     focus: {
-//       // 紐づいている要素がDOMに挿入される時
-//       inserted: function(el) {
-//         // 要素にフォーカスをあてる
-//         el.focus()
-//         console.log(v-focus)
-//       }
-//     }
-//   }
-// })
-
-
-Vue.directive('example', {
-  bind: function(el, binding) {
-    console.log('v-example bind')
+new Vue({
+  el: '#app',
+  data: {
+    video1: false,
+    video2: false
   },
-  inserted: function(el, binding) {
-    console.log('v-example inserted')
-  },
-  update: function(el, binding) {
-    console.log('v-example update')
-  },
-  componentUpdated: function(el, binding) {
-    console.log('v-example componentUpdated')
-  },
-  unbind: function(el, binding) {
-    console.log('v-example unbind')
-  },
+  directives: {
+    video(el, binding) {
+      binding.value? el.play() : el.pause()
+    }
+  }
 })
+
+
