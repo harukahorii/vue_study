0046343f826f5eb609894bc3e311ceb16b0555ae
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-12-26 22:07:30 +0900

    update index12.js

--- main12.js
+++ main12.js
@@ -1,8 +1,15 @@
-Vue.component('my-component', {
-  template: '<p>{{ message }}</p>',
-  data: function() {
-    return {
-      message: 'Hello Vue.js'
-    }
-  }
+// Vue.component('my-component', {
+//   template: '<p>{{ message }}</p>',
+//   data: function() {
+//     return {
+//       message: 'Hello Vue.js'
+//     }
+//   }
+// })
+
+Vue.component('comp-child', {
+  // テンプレートで受け取ったvalを使用
+  template: '<p>{{ val }}</p>',
+  // 受け取る属性名を指定
+  props: ['val']
 })
