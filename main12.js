beb910dead627d3fa4e5fbc83da9debfdc76f413
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2020-01-04 05:25:49 +0900

    update index12.js

--- main12.js
+++ main12.js
@@ -7,9 +7,16 @@
 //   }
 // })
 
-Vue.component('comp-child', {
-  // テンプレートで受け取ったvalを使用
-  template: '<p>{{ val }}</p>',
-  // 受け取る属性名を指定
-  props: ['val']
+// Vue.component('comp-child', {
+//   // テンプレートで受け取ったvalを使用
+//   template: '<p>{{ val }}</p>',
+//   // 受け取る属性名を指定
+//   props: ['val']
+// })
+
+new Vue({
+  data: {
+    valueA: 'これは子A',
+    valueB: 'これは子B'
+  }
 })
