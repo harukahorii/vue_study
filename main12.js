9e6187e202b6ebd8c55d7671cc4d42ad5dcad7af
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2020-01-18 07:51:39 +0900

    update main12.js

--- main12.js
+++ main12.js
@@ -8,6 +8,7 @@ new Vue({
   data: {
     list: [
       {id: 1, name: 'スライム', hp: 100  },
-      {id: 2, name: 'ゴブリン', hp: 100 },
+      {id: 2, name: 'ゴブリン', hp: 200 },
+      {id: 3, name: 'ドラゴン', hp: 500 }
     ]
   } })
