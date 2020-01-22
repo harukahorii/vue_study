014b251bb3de25ce339410ae7471fb109d49e88a
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2020-01-23 07:37:25 +0900

    update index13.html

--- main10.js
+++ main10.js
@@ -1,19 +1,14 @@
 new Vue({
   el: '#app',
   data: {
-    list: []
+    width: 800
   },
-  watch: {
-    list: function() {
-      // 更新後のul要素の高さを取得できない…
-    console.log('通常:', this.$refs.list.offsetHeight)
-    // nextTickを使えばできる
-    this.$nextTick(function() {
-      console.log('nextTick:', this.$refs.list.offsetHeight)
-    })
+  computed: {
+    // 算出プロパティhalfWidthを定義
+    halfWidth: function() {
+      return this.width / 2
     }
   }
 })
 
 
-1
