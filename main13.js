711756d4ebc7c9263a0091654b8f4f10faf8febb
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2020-01-29 21:47:04 +0900

    update index13.html

--- main13.js
+++ main13.js
@@ -1,13 +1,19 @@
-function Cat(name) {
-  this.name = name
-}
-Vue.component('example', {
-  props: {
-    value: Cat
+Vue.component('comp-child', {
+  template: '<li>{{ name }} HP.{{ hp }}\
+    <button v-on:click="doAttack">攻撃する</button></li>',
+  props: { id: Number, name: String, hp: Number },
+  methods: {
+    doAttack: function() {
+      this.$emit('attack', this.id)
+    }
   }
 })
+
 new Vue({
+  el: '#app',
   data: {
-    value: new Cat('たま')
+    list: [
+      
+    ]
   }
 })
