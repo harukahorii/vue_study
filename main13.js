440670ce82aa40bd8f7946a28cb0ee0593128fba
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2020-01-29 21:51:18 +0900

    update main13.js

--- main13.js
+++ main13.js
@@ -13,7 +13,17 @@ new Vue({
   el: '#app',
   data: {
     list: [
-      
+      { id: 1, name: 'スライム', hp: 100 },
+      { id: 2, name: 'ゴブリン', hp: 200 },
+      { id: 3, name: 'ドラゴン', hp: 500 }
     ]
+  },
+  methods: {
+    handleAttack: function(id) {
+      var item = this.list.find(function(el) {
+        return el.id === id
+      })
+      if (item !== underfind && item.hp > 0) item.hp -= 10
+    }
   }
 })
