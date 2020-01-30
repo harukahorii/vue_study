2daab676c13934b524e696db822235f725eca981
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2020-01-30 09:40:35 +0900

    create index14.html

--- main13.js
+++ main13.js
@@ -9,28 +9,6 @@ Vue.component('comp-child', {
   }
 })
 
-// new Vue({
-//   el: '#app',
-//   data: {
-//     list: [
-//       { id: 1, name: 'スライム', hp: 100 },
-//       { id: 2, name: 'ゴブリン', hp: 200 },
-//       { id: 3, name: 'ドラゴン', hp: 500 }
-//     ]
-//   },
-//   methods: {
-//     // attackが発生した！
-//     handleAttack: function (id) {
-//       // 引数のIDから要素を検索
-//       var item = this.list.find(function (el) {
-//         return el.id === id
-//       })
-//       // HPが0より多ければ10減らす
-//       if (item !== undefined && item.hp > 0) item.hp -= 10
-//     }
-//   }
-// })
-
 new Vue({
   el: '#app',
   data: {
