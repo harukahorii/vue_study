64b46475128ab875d265164b5cd5e9f207917ba5
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2020-02-03 22:02:51 +0900

    update main.js

---
+++ main14.js
@@ -0,0 +1,16 @@
+var bus = new Vue ({
+  data: {
+    count: 0
+  }
+})
+Vue.component('component-b', {
+  template: '<p>bus: {{ bus.count }}</p>',
+  computed: {
+    bus: function() { return bus.$data }
+  },
+  created: function() {
+    bus.$on('bus-event', function() {
+      this.count++
+    })
+  }
+})
